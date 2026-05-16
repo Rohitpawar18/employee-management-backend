a7f244fbbf8d1ed200ea2e238ac1d602ce6ec7de
Rohit Pawar <rohitshrikantpawar@gmail.com>
2026-05-16 15:21:10 +0530

    Email send for employee add and delete

---
+++ src/main/java/com/example/employee_management/service/EmailService.java
@@ -0,0 +1,76 @@
+package com.example.employee_management.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    private void sendEmail(String toEmail, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+
+    // Admin mail - new employee added
+    public void sendAdminEmployeeAddedEmail(String adminEmail, String employeeName, String department){
+        sendEmail(
+                adminEmail,
+                "New Employee Added : "+ employeeName,
+                "Hello Admin,\n\n"+
+                "A new employee has been added to the system.\n\n"+
+                "Employee Name : "+employeeName+"\n"+
+                "Department    : "+department+"\n\n"+
+                "Please login to the Employee Management System for more details.\n\n"+
+                "Regards,\nEmployee Management System"
+        );
+    }
+
+    // Employee Email : welcome email
+    public void sendWelcomeEmail(String employeeEmail, String employeeName, String department){
+        sendEmail(
+                employeeEmail,
+                "Welcome to the Company, "+employeeName+"!",
+                "Dear "+employeeName+",\n\n"+
+                "Welcome to company! We are excited to have you on board.\n\n"+
+                "Your profile has been successfully added to our system.\n\n" +
+                "Department : " + department + "\n\n" +
+                "If you have any questions, please contact the HR department.\n\n" +
+                "Best Regards,\nEmployee Management System"
+        );
+    }
+
+    //Admin email : employee deleted
+    public void  sendAdminEmployeeDeletedEmail(String adminEmail, String employeeName, String department){
+        sendEmail(
+                adminEmail,
+                "Employee Removed : "+employeeName,
+                "Hello Admin,\n\n"+
+                "An employee has been removed from the system.\n\n"+
+                "Employee Name : "+employeeName+"\n"+
+                "Department    : "+department+"\n\n"+
+                "Please login to the Employee Management System for more details.\n\n"+
+                "Regards,\nEmployee Management System"
+        );
+    }
+
+    // Emloyee email : removal notification
+    public void sendEmployeeRemovedEmail(String employeeEmail, String employeeName){
+        sendEmail(
+                employeeEmail,
+                "Your Profile Has Been Removed",
+                "Dear "+employeeName+",\n\n"+
+                "We want to inform you that your profile has been removed from our "+
+                "Employee Management System.\n\n"+
+                "If you think this is a mistake, please contact the HR department immediately.\n\n"+
+                "Best Regards,\nEmployee Management System"
+        );
+    }
+}
--- src/main/java/com/example/employee_management/service/EmployeeService.java
+++ src/main/java/com/example/employee_management/service/EmployeeService.java
@@ -2,7 +2,9 @@ package com.example.employee_management.service;
 
 import com.example.employee_management.model.Employee;
 import com.example.employee_management.repository.EmployeeRepository;
+import org.bson.internal.BsonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,16 +14,42 @@ import java.util.Optional;
 public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
+    @Autowired
+    private EmailService emailService;
+
+    @Value("${spring.mail.username}")
+    private String adminEmail;
 
     public Employee createEmployee(Employee employee){
-        return employeeRepository.save(employee);
+
+        Employee saved = employeeRepository.save(employee);
+        try{
+            // Email to Admin
+            emailService.sendAdminEmployeeAddedEmail(
+                    adminEmail,
+                    saved.getName(),
+                    saved.getDepartment()
+            );
+
+            //Welcome Email to Employee
+            emailService.sendWelcomeEmail(
+                    saved.getEmail(),
+                    saved.getName(),
+                    saved.getDepartment()
+            );
+        }catch(Exception e){
+            System.out.println("Email sending failed : "+e.getMessage());
+        }
+        return saved;
     }
 
     public List<Employee> getAllEmployees(){
+
         return employeeRepository.findAll();
     }
 
     public Optional<Employee> getEmployeeById(String id){
+
         return employeeRepository.findById(id);
     }
 
@@ -31,6 +59,27 @@ public class EmployeeService {
     }
 
     public void deleteEmployee(String id){
+
+        Employee employee = employeeRepository.findById(id)
+                        .orElseThrow(() -> new RuntimeException("Employee Not found"));
+
+        try{
+            //Email To Admin
+            emailService.sendAdminEmployeeDeletedEmail(
+                    adminEmail,
+                    employee.getName(),
+                    employee.getDepartment()
+            );
+
+            //Removal Email to Employee
+            emailService.sendEmployeeRemovedEmail(
+                    employee.getEmail(),
+                    employee.getName()
+            );
+        }catch(Exception e){
+            System.out.println("Email Sending Failed : "+e.getMessage());
+        }
+
         employeeRepository.deleteById(id);
     }
 }
